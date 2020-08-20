a6f096ab0f9b384e90733685505f3fbaf5ea7c7f
dgo8914 <dg8390@gmail.com>
2020-08-20 10:59:14 -0500

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,20 +1,20 @@
-#pong game with python3
+
 
 import turtle
 
 #game setup
- board = turtle.Screen()
- board.tittle("welcome friends hope you like it ")
- board.bgcolor("black")
- board.setup(width=800, height=600)
- board.tracer(0)
+board = Turtle.Screen()
+board.tittle("welcome friends hope you like it ")
+board.bgcolor("black")
+board.setup(width=800, height=600)
+board.tracer(0)
 
 #raquet 1
 raquet_1 = turtle.Turtle()
 raquet_1.speed(0)
 raquet_1.shape("square")
 raquet_1.color("red")
-raquet_1.shapesize(stretch_wid=5, stretch_len=1pong)
+raquet_1.shapesize(stretch_wid=5, stretch_len=1)
 raquet_1.penup()
 raquet_1.goto(-350, 0)
 
@@ -24,7 +24,7 @@ raquet_2 = turtle.Turtle()
 raquet_2.speed(0)
 raquet_2.shape("square")
 raquet_2.color("blue")
-raquet_2.shapesize(stretch_wid=5, stretch_len=1pong)
+raquet_2.shapesize(stretch_wid=5, stretch_len=1)
 raquet_2.penup()
 raquet_2.goto(350, 0)
 
@@ -36,7 +36,7 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = 2
-ball.dv = 2
+ball.dy = -2
 
 #movement
 def raquet_1_up:
@@ -77,4 +77,24 @@ while True:
 	#ball movement
 	ball.setx(ball.xcor() + ball.dx)
 	ball.sety(ball.ycor() + ball.dy)
+	
+	#boders board
+	if ball.ycor() > 290:
+		ball.sety(290)
+		ball.dy *= -1
 
+	if ball.ycor() > -290:
+		ball.sety(-290)
+		ball.dy *= -1
+
+	#return ball to mid	
+	if ball.xcor()>390:
+		ball.goto(0, 0)
+		ball.dx *= -1
+
+	if ball.xcor()< -390:
+		ball.goto(0, 0)
+		ball.dx *= -1
+
+	#racquets hit
+	if ball.xcor() >340 and (ball.ycor() < raquet_2l.ycor() + 50 and ball.ycor() > raquet_2.ycor() -50):
